7183d858e452437a3a4737a287389bb3212be3a7
Mac_Developer <araozangel842@gmail.com>
2023-04-25 16:11:16 +0300

    Adding product controller to fetch all ProductController

---
+++ src/main/java/com/example/backend/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package com.example.backend.controllers;
+
+import com.example.backend.model.Products;
+import com.example.backend.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ProductController {
+
+    @Autowired
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Products> getProducts() {
+        return productService.getProducts();
+    }
+}
