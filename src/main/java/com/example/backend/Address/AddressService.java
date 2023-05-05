bc2fa8f1016722c89dfec3b28b72ddc47e0a205d
Mac_Developer <araozangel842@gmail.com>
2023-05-05 21:05:55 +0300

    Add controller and service

---
+++ src/main/java/com/example/backend/Address/AddressService.java
@@ -0,0 +1,43 @@
+package com.example.backend.Address;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@Service
+public class AddressService {
+    @Autowired
+    private AddressRepository addressRepository;
+
+    public List<Address> findAll() {
+        return addressRepository.findAll();
+    }
+
+    public Address findById(UUID id) {
+        return addressRepository.findById(id).orElse(null);
+    }
+
+    public Address createOne(Address address) {
+        return addressRepository.save(address);
+    }
+
+    public Address updateOne(Address address) {
+        Address addressToUpdate = addressRepository.findById(address.getId()).orElse((null));
+        if (addressToUpdate == null) {
+            return null;
+        }
+        addressToUpdate.setAddress(address.getAddress());
+        addressToUpdate.setCity(address.getCity());
+        addressToUpdate.setPostalCode(address.getPostalCode());
+        addressToUpdate.setCountry(address.getCountry());
+        addressToUpdate.setUser(address.getUser());
+        return addressRepository.save(addressToUpdate);
+    }
+
+    public void deleteById(UUID id) {
+        addressRepository.deleteById(id);
+    }
+}
