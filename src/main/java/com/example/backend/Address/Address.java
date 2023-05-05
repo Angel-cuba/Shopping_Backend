3a7a148fa7e5214c92fa2f4fb8613f04a074925c
Mac_Developer <araozangel842@gmail.com>
2023-05-05 21:05:19 +0300

    Add Address Entity

---
+++ src/main/java/com/example/backend/Address/Address.java
@@ -0,0 +1,47 @@
+package com.example.backend.Address;
+
+import com.example.backend.User.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
+    @Column(nullable = false, columnDefinition = "varchar(255)")
+    private String address;
+
+    @Column(nullable = false, columnDefinition = "varchar(255)")
+    private String city;
+
+    @Column(nullable = false, columnDefinition = "varchar(5)")
+    private String postalCode;
+
+    @Column(nullable = false, columnDefinition = "varchar(255)")
+    private String country;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(optional = false)
+    private User user;
+
+}
