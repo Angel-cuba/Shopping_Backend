7838d490c16f4c0ffc069e9d5f9c467bf66f7d04
Mac_Developer <araozangel842@gmail.com>
2023-05-08 03:30:10 +0300

    Add basic CORS configuration.

--- src/main/java/com/example/backend/Products/ProductController.java
+++ src/main/java/com/example/backend/Products/ProductController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
     @Autowired
