33aabd7a15cb40a9c1faf66579cbbdef0d5202dd
Mac_Developer <araozangel842@gmail.com>
2023-05-09 16:47:04 +0300

    Implement signup method

--- src/main/java/com/example/backend/User/UserController.java
+++ src/main/java/com/example/backend/User/UserController.java
@@ -1,6 +1,7 @@
 package com.example.backend.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,31 +11,51 @@ import java.util.UUID;
 @RequestMapping("api/v1/users")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+  @Autowired
+  private UserService userService;
 
-    @GetMapping()
-    public List<User> findAll() {
-        return userService.findAll();
-    }
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
-    @GetMapping("/{id}")
-    public User findById(@PathVariable UUID id) {
-        return userService.findById(id);
-    }
+  @GetMapping()
+  public List<User> findAll() {
+    return userService.findAll();
+  }
 
-    @PostMapping
-    public User createOne(@RequestBody User user) {
-        return userService.createOne(user);
-    }
+  @GetMapping("/{id}")
+  public User findById(@PathVariable UUID id) {
+    return userService.findById(id);
+  }
 
-    @PutMapping
-    public User updateOne(@RequestBody User user) {
-        return userService.updateOne(user);
-    }
+  @PostMapping
+  public User createOne(@RequestBody User user) {
+    return userService.createOne(user);
+  }
 
-    @DeleteMapping("/{id}")
-    public void deleteOne(@PathVariable UUID id) {
-        userService.deleteById(id);
-    }
+  @PostMapping("/signup")
+  public User signup(@RequestBody User user) {
+    User existingUser = userService.findUserByEmail(user.getEmail());
+      if (existingUser != null) {
+         return null;
+      }
+    user.setUsername(user.getUsername());
+    user.setFirstname(user.getFirstname());
+    user.setLastname(user.getLastname());
+    user.setPhone(user.getPhone());
+    user.setEmail(user.getEmail());
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
+    user.setRole(Role.USER);
+    userService.createOne(user);
+    return user;
+  }
+
+  @PutMapping
+  public User updateOne(@RequestBody User user) {
+    return userService.updateOne(user);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteOne(@PathVariable UUID id) {
+    userService.deleteById(id);
+  }
 }
