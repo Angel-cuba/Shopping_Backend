26bddd008d9616bb095ab7df399f10610950f4d9
Mac_Developer <araozangel842@gmail.com>
2023-05-10 22:40:16 +0300

    Add method to validate token

--- src/main/java/com/example/backend/Utils/JwtHelper.java
+++ src/main/java/com/example/backend/Utils/JwtHelper.java
@@ -2,35 +2,65 @@ package com.example.backend.Utils;
 
 import com.example.backend.User.User;
 import io.github.cdimascio.dotenv.Dotenv;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import me.paulschwarz.springdotenv.DotenvPropertyLoader;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtHelper {
-  Dotenv dotenv = Dotenv.load();
+    Dotenv dotenv = Dotenv.load();
 
-   private final String SECRET_KEY = dotenv.get("SECRET_KEY");
-  public String generateToken(User user) {
-    Map<String, Object> claims = new HashMap<>();
-    claims.put("user_id", user.getId());
-    claims.put("username", user.getUsername());
-    claims.put("role", user.getRole());
+    private final String SECRET_KEY = dotenv.get("SECRET_KEY");
 
-    return jwtToken(claims, user.getUsername());
-  }
+    public String generateToken(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("user_id", user.getId());
+        claims.put("username", user.getUsername());
+        claims.put("role", user.getRole());
 
-  private String jwtToken(Map<String, Object> claims, String subject) {
-    return Jwts.builder()
-      .setClaims(claims)
-      .setSubject(subject)
-      .setIssuedAt(new Date(System.currentTimeMillis()))
-      .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-      .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
-  }
+        return jwtToken(claims, user.getUsername());
+    }
+
+    private String jwtToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
 }
