9cccc6aa67b53497af4372e30a51f817dde28faa
Mac_Developer <araozangel842@gmail.com>
2023-05-11 22:49:04 +0300

    Create user wishes entity

---
+++ src/main/java/com/example/backend/WishesList/Wishes.java
@@ -0,0 +1,31 @@
+package com.example.backend.WishesList;
+
+import com.example.backend.User.User;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "wishes")
+public class Wishes {
+  @Id
+  @GeneratedValue
+  @UuidGenerator
+  private UUID id;
+  @Column
+  private List<String> userWishes;
+  @Column
+  private Integer totalOfItems;
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+  @UpdateTimestamp
+  private LocalDateTime modifiedAt;
+
+  @OneToOne(optional = false)
+  private User user;
+}
