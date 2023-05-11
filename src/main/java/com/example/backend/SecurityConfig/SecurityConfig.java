60e644ad5fa302a6165b057cfd73110851831421
Mac_Developer <araozangel842@gmail.com>
2023-05-11 22:46:45 +0300

    Add cors and addFilterBefore annotations

--- src/main/java/com/example/backend/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/example/backend/SecurityConfig/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.backend.SecurityConfig;
 
+import com.example.backend.JwtFilters.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,10 +14,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+  @Autowired
+  private JwtFilter jwtFilter;
   @Bean
   public AuthenticationManager authenticationManagerBean(
           AuthenticationConfiguration authenticationConfiguration
@@ -31,6 +36,8 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
+            .cors()
+            .and()
             .csrf()
             .disable()
             .authorizeHttpRequests().requestMatchers("/api/v1/users/signup", "/api/v1/users/signin")
@@ -41,7 +48,9 @@ public class SecurityConfig {
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
-            .httpBasic(withDefaults()).formLogin();
+            .httpBasic(withDefaults()).formLogin()
+            .and()
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class );
     return http.build();
   }
 }
