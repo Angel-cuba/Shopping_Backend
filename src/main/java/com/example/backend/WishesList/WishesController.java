47be2d4e589889960cb654e3172f8ea3e167e3f7
Mac_Developer <araozangel842@gmail.com>
2023-05-19 18:22:19 +0300

    Methods to handle wishes list(CRUD)

---
+++ src/main/java/com/example/backend/WishesList/WishesController.java
@@ -0,0 +1,48 @@
+package com.example.backend.WishesList;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173"})
+@RestController
+@RequestMapping("/api/v1/wishes")
+public class WishesController {
+
+    private final WishesService wishesService;
+
+    public WishesController(WishesService wishesService) {
+        this.wishesService = wishesService;
+    }
+
+    @GetMapping
+    public Iterable<Wishes> list() {
+        return wishesService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public Wishes get(@PathVariable UUID id) {
+        return wishesService.get(id);
+    }
+
+    @GetMapping("/user/{userId}")
+    public List<Wishes> getWishesByUserId(@PathVariable UUID userId) {
+        return wishesService.getWishesByUserId(userId);
+    }
+
+    @PostMapping
+    public Wishes save(@RequestBody Wishes wishes) {
+       return wishesService.createWishes(wishes);
+    }
+
+    @PutMapping
+    public Wishes update(@RequestBody Wishes wishes) {
+        return wishesService.updateWishes(wishes);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable UUID id) {
+        wishesService.deleteWishListById(id);
+    }
+}
---
+++ src/main/java/com/example/backend/WishesList/WishesService.java
@@ -0,0 +1,49 @@
+package com.example.backend.WishesList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class WishesService {
+    @Autowired
+    private WishesRepository wishesRepository;
+
+    public List<Wishes> listAll() {
+        return wishesRepository.findAll();
+    }
+
+    public Wishes get(UUID id) {
+        if (!wishesRepository.existsById(id)) {
+            return null;
+        } else {
+            return wishesRepository.findById(id).get();
+        }
+    }
+
+    public List<Wishes> getWishesByUserId(UUID userId) {
+        return wishesRepository.findWishedProductsByUserId(userId);
+    }
+
+    public Wishes createWishes(Wishes wishes) {
+        return wishesRepository.save(wishes);
+    }
+
+    public Wishes updateWishes(Wishes wishes) {
+        Wishes wishesToUpdate = wishesRepository.findById(wishes.getId()).orElse(null);
+        if (wishesToUpdate == null) {
+            return null;
+        } else {
+            wishesToUpdate.setUserWishes(wishes.getUserWishes());
+            wishesToUpdate.setTotalOfItems(wishes.getTotalOfItems());
+            wishesToUpdate.setUser(wishes.getUser());
+            return wishesRepository.save(wishesToUpdate);
+        }
+    }
+
+    public void deleteWishListById(UUID id) {
+        wishesRepository.deleteById(id);
+    }
+}
