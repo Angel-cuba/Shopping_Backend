ae4768ab659842b38328f381622fc61a0c58484a
Mac_Developer <araozangel842@gmail.com>
2023-05-19 18:26:24 +0300

    Create method to fetch products of user wishlist

--- src/main/java/com/example/backend/Products/ProductRepository.java
+++ src/main/java/com/example/backend/Products/ProductRepository.java
@@ -3,8 +3,10 @@ package com.example.backend.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Products, UUID> {
+    List<Products> findProductsByIdIn(List<UUID> ids);
 }
