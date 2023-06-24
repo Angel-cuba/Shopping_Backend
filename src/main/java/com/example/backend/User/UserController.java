ea89168ae8d47b50d4f08ddcc04d0bfb0eedd6d1
Mac_Developer <araozangel842@gmail.com>
2023-06-24 03:00:40 +0300

    Refactor. Implement generic type.

--- src/main/java/com/example/backend/User/UserController.java
+++ src/main/java/com/example/backend/User/UserController.java
@@ -3,8 +3,11 @@ package com.example.backend.User;
 import com.example.backend.Authentication.AuthRequest;
 import com.example.backend.Utils.JwtHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,20 +34,23 @@ public class UserController {
   }
 
   @GetMapping("/{id}")
-  public User findById(@PathVariable UUID id) {
-    return userService.findById(id);
-  }
-
-  @PostMapping
-  public User createOne(@RequestBody User user) {
-    return userService.createOne(user);
+  public ResponseEntity<User> findById(@PathVariable UUID id) {
+    User user = userService.findById(id);
+    if (user == null) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<>(user, HttpStatus.OK);
   }
 
   @PostMapping("/signup")
-  public String signup(@RequestBody User user) {
+  public ResponseEntity<String> signup(@RequestBody User user) {
     User existingUser = userService.findUserByEmail(user.getEmail());
     if (existingUser != null) {
-      return null;
+      return ResponseEntity.badRequest().body("Email already exists");
+    }
+    existingUser = userService.findUserName(user.getUsername());
+    if (existingUser != null) {
+      return ResponseEntity.badRequest().body("Username already exists");
     }
     user.setUsername(user.getUsername());
     user.setFirstname(user.getFirstname());
@@ -54,20 +60,32 @@ public class UserController {
     user.setPassword(passwordEncoder.encode(user.getPassword()));
     user.setRole(Role.USER);
     userService.createOne(user);
-    return jwtHelper.generateToken(user);
+    String token = jwtHelper.generateToken(user);
+    return ResponseEntity.ok(token);
   }
 
   @PostMapping("/signin")
-  public String login(@RequestBody AuthRequest authRequest) {
-    authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(
-                    authRequest.getUsername(),
-                    authRequest.getPassword()
-            )
-    );
+  public ResponseEntity<String> login(@RequestBody AuthRequest authRequest) {
+    try {
+      authenticationManager.authenticate(
+              new UsernamePasswordAuthenticationToken(
+                      authRequest.getUsername(),
+                      authRequest.getPassword()
+              )
+      );
+    } catch (AuthenticationException e) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+    }
+
     User user = userService.findUserName(authRequest.getUsername());
-      return jwtHelper.generateToken(user);
+    if (user == null) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not found");
+    }
+
+    String token = jwtHelper.generateToken(user);
+    return ResponseEntity.ok(token);
   }
+
 
   @PutMapping
   public User updateOne(@RequestBody User user) {
